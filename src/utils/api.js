c13070f30983b446603482c25fbf4ddc7ebaeb02
vikash <vp3637637@gmail.com>
2024-07-26 06:44:59 +0530

    api key protected

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 
-
+const apikey= process.env.API_KEY
 const options = {
     method: 'GET',
     url: 'https://real-time-amazon-data.p.rapidapi.com/search',
@@ -12,7 +12,7 @@ const options = {
       country: 'US'
     },
     headers: {
-      'X-RapidAPI-Key': '59a2f37d10msh5e0c2313caf714ep125022jsn34d1b7daea85',
+      'X-RapidAPI-Key':apikey,
       'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
     }
   };
